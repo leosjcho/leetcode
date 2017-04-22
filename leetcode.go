fc4c011aef3ea318fae8121cdf7898c2df4c3cc1
Leo Cho <bsjcho@gmail.com>
2017-04-22 16:24:13 -0700

    brute force string search

--- leetcode.go
+++ leetcode.go
@@ -224,5 +224,17 @@ func lengthOfLongestSubstring(s string) int {
 */
 
 func strStr(haystack string, needle string) int {
-
+	if len(needle) == 0 {
+		return 0
+	}
+	for i := 0; i <= len(haystack)-len(needle); i++ {
+		for j := 0; j < len(needle); j++ {
+			if haystack[i+j] != needle[j] {
+				break
+			} else if j == len(needle)-1 {
+				return i
+			}
+		}
+	}
+	return -1
 }

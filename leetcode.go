4b8dc11b5b79190da9bf14133436c5533077d0d9
Leo Cho <bsjcho@gmail.com>
2017-04-23 12:50:05 -0700

    path sum

--- leetcode.go
+++ leetcode.go
@@ -490,3 +490,26 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
 	}
 	return false
 }
+
+/*
+112. Path Sum
+*/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func hasPathSum(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Left == nil && root.Right == nil {
+		return sum == root.Val
+	}
+	newSum := sum - root.Val
+	return hasPathSum(root.Left, newSum) || hasPathSum(root.Right, newSum)
+}

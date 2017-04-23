9d053f39499ec1c698ec3d7342755b9eba2f3dc9
Leo Cho <bsjcho@gmail.com>
2017-04-22 20:29:20 -0700

    reverse linked list

--- leetcode.go
+++ leetcode.go
@@ -364,3 +364,30 @@ func myAtoi(str string) int {
 	}
 	return base * sign
 }
+
+/*
+206. Reverse Linked List
+*/
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	return reverseListHelper(head, nil)
+}
+
+func reverseListHelper(node *ListNode, p *ListNode) *ListNode {
+	next := node.Next
+	node.Next = p
+	if next == nil {
+		return node
+	}
+	return reverseListHelper(next, node)
+}

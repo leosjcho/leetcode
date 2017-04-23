3f54e4f1c6c9b28e127cd793ba0dd30919c18352
Leo Cho <bsjcho@gmail.com>
2017-04-23 01:21:42 -0700

    100. Same Tree

--- leetcode.go
+++ leetcode.go
@@ -465,3 +465,28 @@ func maxDepth(root *TreeNode) int {
 	}
 	return maxRight + 1
 }
+
+/*
+100. Same Tree
+*/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	if p.Val == q.Val {
+		return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+	}
+	return false
+}

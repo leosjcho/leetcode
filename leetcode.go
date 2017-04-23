1c38178550572f0bfdf2296ac236fdf2065dcde1
Leo Cho <bsjcho@gmail.com>
2017-04-23 01:15:27 -0700

    104. Maximum Depth of Binary Tree

--- leetcode.go
+++ leetcode.go
@@ -442,5 +442,26 @@ func swapPairs(head *ListNode) *ListNode {
 	return child
 }
 
+/*
+104. Maximum Depth of Binary Tree
+*/
 
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	maxLeft := maxDepth(root.Left)
+	maxRight := maxDepth(root.Right)
+	if maxLeft > maxRight {
+		return maxLeft + 1
+	}
+	return maxRight + 1
 }

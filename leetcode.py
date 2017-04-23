2224b61707d36f46eda659e460c4f36e6217ff2b
Leo Cho <bsjcho@gmail.com>
2017-04-22 20:53:40 -0700

    linked list cycle check

--- leetcode.py
+++ leetcode.py
@@ -426,3 +426,34 @@ class Solution(object):
         """
         node.val = node.next.val
         node.next = node.next.next
+
+'''
+141. Linked List Cycle
+'''
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head is None or head.next is None:
+            return False
+        n1 = head
+        n2 = head.next
+        while True:
+            if n1.val == n2.val:
+                return True
+            if n1.next is None or n2.next is None:
+                return False
+            n1 = n1.next
+            n2 = n2.next
+            if n2.next is None:
+                return False
+            n2 = n2.next

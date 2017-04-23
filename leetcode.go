99bf8d355633b03433c9aaf9aa3fd7fbc6317990
Leo Cho <bsjcho@gmail.com>
2017-04-23 13:44:02 -0700

    binary tree paths

--- leetcode.go
+++ leetcode.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"math"
+	"strconv"
 	"strings"
 	"unicode"
 )
@@ -512,4 +513,39 @@ func hasPathSum(root *TreeNode, sum int) bool {
 	}
 	newSum := sum - root.Val
 	return hasPathSum(root.Left, newSum) || hasPathSum(root.Right, newSum)
+}
+
+/*
+257. Binary Tree Paths
+*/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+/*
+edge cases
+empty root
+1 child root
+*/
+
+func binaryTreePaths(root *TreeNode) []string {
+	if root == nil {
+		return []string{}
+	}
+	strVal := strconv.Itoa(root.Val)
+	if root.Left == nil && root.Right == nil {
+		return []string{strVal}
+	}
+	mergedPaths := append(binaryTreePaths(root.Left), binaryTreePaths(root.Right)...)
+	paths := []string{}
+	for _, path := range mergedPaths {
+		paths = append(paths, strVal+"->"+path)
+	}
+	return paths
 }

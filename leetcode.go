214145bc7aad15a0e7ecc59b607ace0c4536e5b4
Leo Cho <bsjcho@gmail.com>
2017-04-24 01:30:08 -0700

    35. Search Insert Position

--- leetcode.go
+++ leetcode.go
@@ -859,3 +859,23 @@ func isPowerOfTwo(n int) bool {
 	}
 	return count == 1
 }
+
+/*
+35. Search Insert Position
+*/
+
+func searchInsert(nums []int, target int) int {
+	lo, hi := 0, len(nums)-1
+	for lo <= hi {
+		mid := ((hi - lo) / 2) + lo
+		if nums[mid] == target {
+			return mid
+		}
+		if target < nums[mid] {
+			hi = mid - 1
+		} else {
+			lo = mid + 1
+		}
+	}
+	return lo
+}

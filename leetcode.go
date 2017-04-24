4d4834f52b00e7db02cf9aa1e8a735dfe7b590e4
Leo Cho <bsjcho@gmail.com>
2017-04-24 00:19:19 -0700

    231. Power of Two

--- leetcode.go
+++ leetcode.go
@@ -841,3 +841,21 @@ func reverse(x int) int {
 	}
 	return result
 }
+
+/*
+231. Power of Two
+*/
+
+func isPowerOfTwo(n int) bool {
+	if n < 0 {
+		return false
+	}
+	count := 0
+	var i uint
+	for i = 0; i < 32; i++ {
+		if ((1 << i) & n) != 0 {
+			count++
+		}
+	}
+	return count == 1
+}

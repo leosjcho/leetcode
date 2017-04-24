575c6a1de778ff9484a8bc0e8cf081f4c71c80da
Leo Cho <bsjcho@gmail.com>
2017-04-23 20:16:44 -0700

    20. Valid Parentheses

--- leetcode.go
+++ leetcode.go
@@ -8,6 +8,78 @@ import (
 )
 
 /*
+Data Structures
+*/
+
+type Queue struct {
+	items []int
+}
+
+func NewQueue() *Queue {
+	return &Queue{items: []int{}}
+}
+
+func (q *Queue) Push(x int) {
+	q.items = append(q.items, x)
+}
+
+func (q *Queue) Peek() int {
+	if q.Size() == 0 {
+		return -1
+	}
+	return q.items[0]
+}
+
+func (q *Queue) Pop() int {
+	if q.Size() == 0 {
+		return -1
+	}
+	x := q.items[0]
+	q.items = q.items[1:]
+	return x
+}
+
+func (q *Queue) Size() int {
+	return len(q.items)
+}
+
+func (q *Queue) IsEmpty() bool {
+	return len(q.items) == 0
+}
+
+type AStack struct {
+	items []string
+}
+
+func NewAStack() *AStack {
+	return &AStack{[]string{}}
+}
+
+func (a *AStack) Push(s string) {
+	a.items = append(a.items, s)
+}
+
+func (a *AStack) Pop() string {
+	if len(a.items) == 0 {
+		return ""
+	}
+	val := a.items[len(a.items)-1]
+	a.items = a.items[:len(a.items)-1]
+	return val
+}
+
+func (a *AStack) Peek() string {
+	if len(a.items) == 0 {
+		return ""
+	}
+	return a.items[len(a.items)-1]
+}
+
+func (a *AStack) Size() int {
+	return len(a.items)
+}
+
+/*
 461. Hamming Distance
 */
 
@@ -629,38 +701,32 @@ func (this *MyStack) Empty() bool {
  * param_4 := obj.Empty();
  */
 
-type Queue struct {
-	items []int
-}
+/*
+20. Valid Parentheses
+*/
 
-func NewQueue() *Queue {
-	return &Queue{items: []int{}}
-}
-
-func (q *Queue) Push(x int) {
-	q.items = append(q.items, x)
-}
-
-func (q *Queue) Peek() int {
-	if q.Size() == 0 {
-		return -1
+func isValid(s string) bool {
+	stack := NewAStack()
+	for _, c := range s {
+		sc := string(c)
+		if strings.Contains("({[", sc) {
+			stack.Push(sc)
+		} else {
+			if stack.Peek() != matchingOpen(sc) {
+				return false
+			}
+			stack.Pop()
+		}
 	}
-	return q.items[0]
+	return stack.Size() == 0
 }
 
-func (q *Queue) Pop() int {
-	if q.Size() == 0 {
-		return -1
+func matchingOpen(s string) string {
+	if s == ")" {
+		return "("
+	} else if s == "}" {
+		return "{"
+	} else {
+		return "["
 	}
-	x := q.items[0]
-	q.items = q.items[1:]
-	return x
-}
-
-func (q *Queue) Size() int {
-	return len(q.items)
-}
-
-func (q *Queue) IsEmpty() bool {
-	return len(q.items) == 0
 }

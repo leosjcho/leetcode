7299522eb2c829ad92a7ca23b33719dbd141f8ed
Leo Cho <bsjcho@gmail.com>
2017-04-24 15:13:51 -0700

    367. Valid Perfect Square

--- leetcode.go
+++ leetcode.go
@@ -910,3 +910,23 @@ func search(nums []int, target int, success func(target int, nums []int, mid int
 	}
 	return -1
 }
+
+/*
+367. Valid Perfect Square
+*/
+
+func isPerfectSquare(num int) bool {
+	lo, hi := 0, math.MaxUint32
+	for lo <= hi {
+		mid := (hi-lo)/2 + lo
+		if num == mid*mid {
+			return true
+		}
+		if num < mid*mid {
+			hi = mid - 1
+		} else {
+			lo = mid + 1
+		}
+	}
+	return false
+}

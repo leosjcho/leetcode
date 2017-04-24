892ef492a5af2dd2cf6b7a0f07ae609fab5eed6d
Leo Cho <bsjcho@gmail.com>
2017-04-23 21:49:58 -0700

    232. Implement Queue using Stacks

--- leetcode.go
+++ leetcode.go
@@ -79,6 +79,38 @@ func (a *AStack) Size() int {
 	return len(a.items)
 }
 
+type IStack struct {
+	items []int
+}
+
+func NewIStack() *IStack {
+	return &IStack{[]int{}}
+}
+
+func (s *IStack) Push(x int) {
+	s.items = append(s.items, x)
+}
+
+func (s *IStack) Pop() int {
+	if len(s.items) == 0 {
+		return -1
+	}
+	val := s.items[len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
+	return val
+}
+
+func (s *IStack) Peek() int {
+	if len(s.items) == 0 {
+		return -1
+	}
+	return s.items[len(s.items)-1]
+}
+
+func (s *IStack) Size() int {
+	return len(s.items)
+}
+
 /*
 461. Hamming Distance
 */
@@ -730,3 +762,52 @@ func matchingOpen(s string) string {
 		return "["
 	}
 }
+
+/*
+232. Implement Queue using Stacks
+*/
+
+type MyQueue struct {
+	input  *IStack
+	output *IStack
+}
+
+/** Initialize your data structure here. */
+func Constructor() MyQueue {
+	return MyQueue{NewIStack(), NewIStack()}
+}
+
+/** Push element x to the back of queue. */
+func (this *MyQueue) Push(x int) {
+	this.input.Push(x)
+}
+
+/** Removes the element from in front of queue and returns that element. */
+func (this *MyQueue) Pop() int {
+	this.Peek()
+	return this.output.Pop()
+}
+
+/** Get the front element. */
+func (this *MyQueue) Peek() int {
+	if this.output.Size() == 0 {
+		for this.input.Size() > 0 {
+			this.output.Push(this.input.Pop())
+		}
+	}
+	return this.output.Peek()
+}
+
+/** Returns whether the queue is empty. */
+func (this *MyQueue) Empty() bool {
+	return this.input.Size() == 0 && this.output.Size() == 0
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Peek();
+ * param_4 := obj.Empty();
+ */

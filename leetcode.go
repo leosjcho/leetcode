47e034b8014872a6bdcac777c4479810e854bf33
Leo Cho <bsjcho@gmail.com>
2017-04-23 22:47:40 -0700

    136. Single Number

--- leetcode.go
+++ leetcode.go
@@ -811,3 +811,15 @@ func (this *MyQueue) Empty() bool {
  * param_3 := obj.Peek();
  * param_4 := obj.Empty();
  */
+
+/*
+136. Single Number
+*/
+
+func singleNumber(nums []int) int {
+	v := nums[0]
+	for i := 1; i < len(nums); i++ {
+		v = v ^ nums[i]
+	}
+	return v
+}

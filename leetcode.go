4772e7730af75ce89324f1d5c920894fe87cbd8a
Leo Cho <bsjcho@gmail.com>
2017-04-24 00:02:37 -0700

    7. Reverse Integer

--- leetcode.go
+++ leetcode.go
@@ -823,3 +823,21 @@ func singleNumber(nums []int) int {
 	}
 	return v
 }
+
+/*
+7. Reverse Integer
+*/
+
+func reverse(x int) int {
+	result := 0
+	for x != 0 {
+		tail := x % 10
+		nResult := result*10 + tail
+		if nResult > math.MaxInt32 || nResult < math.MinInt32 {
+			return 0
+		}
+		result = nResult
+		x /= 10
+	}
+	return result
+}

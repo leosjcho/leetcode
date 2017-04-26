5ea2e4d9e26e1ad5eb45a33ee9fb44e5d4dea6b2
Leo Cho <bsjcho@gmail.com>
2017-04-26 02:17:43 -0700

    1. Two Sum

--- leetcode.go
+++ leetcode.go
@@ -930,3 +930,36 @@ func isPerfectSquare(num int) bool {
 	}
 	return false
 }
+
+/*
+1. Two Sum
+*/
+
+func twoSum(nums []int, target int) []int {
+	// complement map
+	// keep track of complements seen
+	// linear time and space complexity
+	complements := map[int]int{}
+	for i, x := range nums {
+		complement := target - x
+		j, ok := complements[complement]
+		if ok {
+			return []int{j, i}
+		} else {
+			complements[x] = i
+		}
+	}
+	return []int{}
+}
+
+// try with 1 2 3 4
+// target = 5
+// 0, 1. complement = 5 - 1 = 4
+// complements[4] == nil
+// complements[1] = 0
+// i = 1, x = 2. complement = 5 - 2 = 3
+// complements[3] == nil
+// complements[2] = 1
+// i = 2, x = 3. complements = 5-3=2
+// complements [2] = 1
+// returns [1, 2]

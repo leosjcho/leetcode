41f0a2fcd0e44ea82882062f648e7879293f8513
Leo Cho <bsjcho@gmail.com>
2017-04-26 02:50:53 -0700

    292. Nim Game

--- leetcode.go
+++ leetcode.go
@@ -963,3 +963,21 @@ func twoSum(nums []int, target int) []int {
 // i = 2, x = 3. complements = 5-3=2
 // complements [2] = 1
 // returns [1, 2]
+
+/*
+292. Nim Game
+*/
+
+/*
+if n == 1, 2, or 3, then we win!
+if n == 4, what happens? we lose!
+if n == 5? we win!
+if n == 6? we win!
+if n == 7? we win!
+if n == 8? we lose!
+etc...
+*/
+
+func canWinNim(n int) bool {
+	return n%4 != 0
+}

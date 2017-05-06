9508539f8073fdabd2e9287006fbb01af25aad03
Leo Cho <bsjcho@gmail.com>
2017-05-06 01:21:02 -0700

    121. Best Time to Buy and Sell Stock

--- leetcode.go
+++ leetcode.go
@@ -1503,3 +1503,17 @@ func climbStairsHelper(n int, memo []int) int {
 	memo[n] = climbStairsHelper(n-1, memo) + climbStairsHelper(n-2, memo)
 	return memo[n]
 }
+
+/*
+121. Best Time to Buy and Sell Stock
+*/
+
+func maxProfit(prices []int) int {
+	maxProfit := 0
+	minSeen := math.MaxInt32
+	for _, p := range prices {
+		maxProfit = max(maxProfit, p-minSeen)
+		minSeen = min(minSeen, p)
+	}
+	return maxProfit
+}

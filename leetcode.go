ca0e2646da447a6b2a1b0ee4342adb2a53dacd2e
Leo Cho <bsjcho@gmail.com>
2017-05-08 16:55:27 -0700

    108. Convert Sorted Array to Binary Search Tree

--- leetcode.go
+++ leetcode.go
@@ -1644,3 +1644,27 @@ func dfsHeight(node *TreeNode) int {
 	}
 	return max(leftHeight, rightHeight) + 1
 }
+
+/*
+108. Convert Sorted Array to Binary Search Tree
+*/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	mid := (len(nums) - 1) / 2
+	n := &TreeNode{Val: nums[mid]}
+	n.Left = sortedArrayToBST(nums[0:mid])
+	n.Right = sortedArrayToBST(nums[mid+1 : len(nums)])
+	return n
+}

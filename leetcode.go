6a7a4e287e5ddca71d9cf4afad25e6f6589f9535
Leo Cho <bsjcho@gmail.com>
2017-05-10 15:14:55 -0700

    formatting

--- leetcode.go
+++ leetcode.go
@@ -1984,11 +1984,13 @@ func init2DByteSlice(rows, cols int) [][]byte {
 */
 
 /*
-Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+Given a non-negative integer num, repeatedly add all its digits until the
+result has only one digit.
 
 For example:
 
-Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only
+one digit, return it.
 
 Follow up:
 Could you do it without any loop/recursion in O(1) runtime?

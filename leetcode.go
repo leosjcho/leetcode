accb202bd612fff02b0a73ae33633d59fed7f1f4
Leo Cho <bsjcho@gmail.com>
2017-05-16 00:07:26 -0700

    14. Longest Common Prefix

--- leetcode.go
+++ leetcode.go
@@ -2037,3 +2037,28 @@ func countBits(num int) []int {
 	}
 	return dp
 }
+
+/*
+14. Longest Common Prefix
+*/
+
+func longestCommonPrefix(strs []string) string {
+	lcp := []byte{}
+	var curr byte
+	i := 0
+Loop:
+	for {
+		if len(strs) == 0 || i == len(strs[0]) {
+			break
+		}
+		curr = strs[0][i]
+		for _, str := range strs {
+			if i == len(str) || curr != str[i] {
+				break Loop
+			}
+		}
+		lcp = append(lcp, curr)
+		i++
+	}
+	return string(lcp)
+}

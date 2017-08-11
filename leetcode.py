0e9bc15d2b3c32f370611539f2fd053956b2a0e6
Leo Cho <bsjcho@gmail.com>
2017-08-10 19:05:44 -0700

    155. Min Stack

--- leetcode.py
+++ leetcode.py
@@ -505,3 +505,52 @@ class MovingAverage(object):
 # Your MovingAverage object will be instantiated and called as such:
 # obj = MovingAverage(size)
 # param_1 = obj.next(val)
+
+'''
+155. Min Stack
+'''
+
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.items = []
+        self.mins = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        self.items.append(x)
+        if len(self.mins) == 0 or x <= self.mins[-1]:
+            self.mins.append(x)
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        poppedItem = self.items.pop()
+        if len(self.mins) > 0 and poppedItem == self.mins[-1]:
+            self.mins.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.items[-1] if len(self.items) > 0 else 0
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.mins[-1] if len(self.mins) > 0 else 0
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()

afe0f3bbdae50c970b292d9ee566e947150dcaa8
Leo Cho <bsjcho@gmail.com>
2017-08-10 19:27:58 -0700

    20. Valid Parentheses

--- leetcode.py
+++ leetcode.py
@@ -554,3 +554,25 @@ class MinStack(object):
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+'''
+20. Valid Parentheses
+'''
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        brackets = {")":"(","}":"{","]":"["}
+        for char in s:
+            if char in brackets.values():
+                stack.append(char)
+            elif char in brackets.keys():
+                if len(stack) > 0 and stack[-1] == brackets[char]:
+                    stack.pop()
+                else:
+                    return False
+        return len(stack) == 0

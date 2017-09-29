bb0aa670e7404db5d16020e1458d6cab96315633
Leo Cho <bsjcho@gmail.com>
2017-09-28 20:26:16 -0700

    110. Balanced Binary Tree

--- leetcode.py
+++ leetcode.py
@@ -704,3 +704,37 @@ class Solution(object):
             tail = tail.next
         tail.next = l1 or l2
         return m.next
+
+
+'''
+110. Balanced Binary Tree
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        height = self.isBalancedHelper(root, 0)
+        return height >= 0
+
+    def isBalancedHelper(self, root, height):
+        '''
+        :type root: TreeNode, height: int
+        :rtype height: int
+        '''
+        if not root:
+            return height
+        lh = self.isBalancedHelper(root.left, height+1)
+        rh = self.isBalancedHelper(root.right, height+1)
+        if lh == -1 or rh == -1 or abs(lh-rh) > 1:
+            return -1
+        return max(lh, rh)

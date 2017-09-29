95b4694245ddd18274d10a2baffac9e1bb4cfbcd
Leo Cho <bsjcho@gmail.com>
2017-09-28 18:22:35 -0700

    iterative merge sorted lists

--- leetcode.py
+++ leetcode.py
@@ -680,13 +680,27 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+        '''
+        # recursive
         if not l1 or not l2:
             return l1 or l2
-        m = ListNode(0)
+        m = ListNode(0) # dummy head
         if l1.val < l2.val:
             m.next = l1
             m.next.next = self.mergeTwoLists(l1.next, l2)
         else:
             m.next = l2
             m.next.next = self.mergeTwoLists(l1, l2.next)
+        return m.next
+        '''
+        # iterative
+        m = ListNode(0)
+        tail = m
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next, l1 = l1, l1.next
+            else:
+                tail.next, l2 = l2, l2.next
+            tail = tail.next
+        tail.next = l1 or l2
         return m.next

fa0dbee8be7ae85ddf7e83d0c5eb6872dd5958a5
Leo Cho <bsjcho@gmail.com>
2017-10-02 16:47:39 -0700

    56. Merge Intervals

--- leetcode.py
+++ leetcode.py
@@ -883,3 +883,48 @@ class Solution(object):
 
         max_3, min_2 = heapq.nlargest(3, nums), heapq.nsmallest(2, nums)
         return max(reduce(mul, max_3), reduce(mul, min_2 + [max_3[0]]))
+
+'''
+56. Merge Intervals
+'''
+
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """
+        def overlap(t1, t2):
+            '''
+            :type t1: Interval, t2: Interval
+            :rtype: Interval
+            '''
+            if t2.start < t1.start:
+                t1, t2 = t2, t1
+            if t2.start > t1.end:
+                return None
+            else:
+                return Interval(t1.start, max(t1.end, t2.end))
+
+        if len(intervals) < 2:
+            return intervals
+        intervals.sort(key=lambda x: (x.start, x.end))
+        oi = 0
+        cur = intervals[0]
+        for interval in intervals[1:]:
+            new_interval = overlap(cur, interval)
+            if new_interval:
+                cur = new_interval
+            else:
+                intervals[oi] = cur
+                oi += 1
+                cur = interval
+        intervals[oi] = cur
+        oi += 1
+        return intervals[:oi]

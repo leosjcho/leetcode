22b789de7fd46aa86a7abaa93fab1e5a7eb85e5e
Leo Cho <bsjcho@gmail.com>
2017-10-01 22:48:35 -0700

    121. Best Time to Buy and Sell Stock

--- leetcode.py
+++ leetcode.py
@@ -738,3 +738,22 @@ class Solution(object):
         if lh == -1 or rh == -1 or abs(lh-rh) > 1:
             return -1
         return max(lh, rh)
+
+'''
+121. Best Time to Buy and Sell Stock
+'''
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) <= 1:
+            return 0
+        max_profit = float("-inf")
+        min_seen = prices[0]
+        for i, price in enumerate(prices[1:]):
+            max_profit = max(max_profit, price - min_seen)
+            min_seen = min(min_seen, price)
+        return max(max_profit, 0)

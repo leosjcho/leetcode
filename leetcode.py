06b1729e6a23efd3ff5560d5678b61ea3a02e8e9
Leo Cho <bsjcho@gmail.com>
2017-10-03 16:47:28 -0700

    518. Coin Change 2

--- leetcode.py
+++ leetcode.py
@@ -943,3 +943,23 @@ class Solution(object):
             else:
                 out.append(interval)
         return out
+
+'''
+518. Coin Change 2
+'''
+
+class Solution(object):
+    def change(self, amount, coins):
+        """
+        :type amount: int
+        :type coins: List[int]
+        :rtype: int
+        """
+        combinations = [0] * (amount + 1)
+        combinations[0] = 1
+
+        for coin in coins:
+            for x in range(coin, amount+1):
+                remainder = x - coin
+                combinations[x] += combinations[remainder]
+        return combinations[amount]

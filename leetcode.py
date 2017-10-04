fb5dd4f3918acaadac289f0f45855a9175ba31c4
Leo Cho <bsjcho@gmail.com>
2017-10-04 03:06:07 -0700

    98. Validate Binary Search Tree

--- leetcode.py
+++ leetcode.py
@@ -1028,3 +1028,30 @@ class Solution(object):
         return Line(line2.start,
                     min(line2.length,
                         line1.start+line1.length - line2.start))
+
+'''
+98. Validate Binary Search Tree
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        return self.isValidBSTHelper(root, float('-inf'), float('inf'))
+
+    def isValidBSTHelper(self, root, minval, maxval):
+        if not root:
+            return True
+        if root.val <= minval or root.val >= maxval:
+            return False
+        return (self.isValidBSTHelper(root.left, minval, root.val)
+                and self.isValidBSTHelper(root.right, root.val, maxval))

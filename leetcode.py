f1331c30da4212ead655d6f93ef627678b8871f2
Leo Cho <bsjcho@gmail.com>
2017-10-05 23:49:54 -0700

    200. Number of Islands

--- leetcode.py
+++ leetcode.py
@@ -1189,3 +1189,39 @@ class Solution(object):
             return max(maxDepthHelper(root.left, depth+1),
                        maxDepthHelper(root.right, depth+1))
         return maxDepthHelper(root, 0)
+
+'''
+200. Number of Islands
+'''
+
+import collections
+
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        WATER, LAND = map(str, range(2))
+        Coord = collections.namedtuple("Coord", ("x", "y"))
+
+        def mark_island_dfs(loc, visited):
+            if loc.x < 0 or loc.x >= len(grid) \
+                or loc.y < 0 or loc.y >= len(grid[0]):
+                return
+            if grid[loc.x][loc.y] == LAND and loc not in visited:
+                visited.add(loc)
+                for d in (0,1), (1,0), (-1,0), (0,-1):
+                    new_x, new_y = loc.x + d[0], loc.y + d[1]
+                    mark_island_dfs(Coord(new_x, new_y), visited)
+
+        visited = set()
+        island_count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                loc = Coord(i, j)
+                if loc not in visited and grid[i][j] == LAND:
+                    island_count += 1
+                    mark_island_dfs(loc, visited)
+                    visited.add(loc)
+        return island_count

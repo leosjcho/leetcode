8c39bf5b6a6dc335dbddf7000830364d86f489dc
Leo Cho <bsjcho@gmail.com>
2017-10-07 02:38:32 -0700

    292. Nim Game

--- leetcode.py
+++ leetcode.py
@@ -1275,3 +1275,15 @@ class Solution(object):
             return p.val == q.val and self.isSameTree(p.left, q.left) and \
                 self.isSameTree(p.right, q.right))
         return p is q
+
+'''
+292. Nim Game
+'''
+
+class Solution(object):
+    def canWinNim(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return not n % 4 == 0

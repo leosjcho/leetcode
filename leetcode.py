132b57ec16d55b1b6b51752a9309b7d65ff929f0
Leo Cho <bsjcho@gmail.com>
2017-10-13 00:36:33 -0700

    344. Reverse String

--- leetcode.py
+++ leetcode.py
@@ -1630,3 +1630,21 @@ class LRUCache(object):
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
+'''
+344. Reverse String
+'''
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        A = list(s)
+        l, r = 0, len(s)-1
+        while l < r:
+            A[l], A[r] = A[r], A[l]
+            l += 1
+            r -= 1
+        return "".join(A)

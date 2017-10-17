4e1233cd877d3cca628cf2aa0bac81b12bc2c2d0
Leo Cho <bsjcho@gmail.com>
2017-10-17 13:38:15 -0700

    longest substring w/o repeating chars

--- leetcode.py
+++ leetcode.py
@@ -1801,3 +1801,23 @@ class Solution(object):
             a = digits & mask
             b = carryover & mask
         return a if a <= MAX else ~(a ^ mask)
+
+'''
+3. Longest Substring Without Repeating Characters
+'''
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        start = curmax = 0
+        seen = {}
+        for i, c in enumerate(s):
+            if c in seen and start <= seen[c]:
+                start = seen[c] + 1
+            else:
+                curmax = max(curmax, i - start + 1)
+            seen[c] = i
+        return curmax

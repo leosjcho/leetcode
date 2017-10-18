7a5a598eac77b74f87db168363c98fcbb9950497
Leo Cho <bsjcho@gmail.com>
2017-10-17 19:01:42 -0700

    11. Container With Most Water

--- leetcode.py
+++ leetcode.py
@@ -1911,4 +1911,24 @@ class Solution(object):
         :rtype: void Do not return anything, modify node in-place instead.
         """
         node.val, node.next = node.next.val, node.next.next
-        
+
+'''
+11. Container With Most Water
+'''
+
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        i, j = 0, len(height)-1
+        maxArea = 0
+        while i < j:
+            area = min(height[i], height[j]) * (j - i)
+            maxArea = max(maxArea, area)
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+        return maxArea

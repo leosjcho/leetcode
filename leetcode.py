e69c544f33304506030cb4a70da9a24bbf7842a0
Leo Cho <bsjcho@gmail.com>
2017-10-17 22:12:36 -0700

    7. Reverse Integer

--- leetcode.py
+++ leetcode.py
@@ -1955,3 +1955,23 @@ class Solution(object):
                 return depth
             return max(maxDepthHelper(root.left, depth+1), maxDepthHelper(root.right, depth+1))
         return maxDepthHelper(root, 0)
+
+'''
+7. Reverse Integer
+'''
+
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        max32 = 0x7fffffff
+        rev = 0
+        isNeg = x < 0
+        x = abs(x)
+        while x != 0:
+            rev, x = rev * 10 + x % 10, x // 10
+        if rev > max32:
+            return 0
+        return rev * -1 if isNeg else rev

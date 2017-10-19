910f2f6ce7c549e4250afc739797094fdb34278f
Leo Cho <bsjcho@gmail.com>
2017-10-19 15:29:31 -0700

    111. Minimum Depth of Binary Tree

--- leetcode.py
+++ leetcode.py
@@ -1976,3 +1976,35 @@ class Solution(object):
         if rev > max32:
             return 0
         return rev * -1 if isNeg else rev
+
+'''
+111. Minimum Depth of Binary Tree
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+import collections
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        q = collections.deque()
+        q.append((root, 1))
+        while q:
+            n, d = q.popleft()
+            if not n.left and not n.right:
+                return d
+            if n.left:
+                q.append((n.left, d+1))
+            if n.right:
+                q.append((n.right, d+1))

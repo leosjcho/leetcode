01f85fa411145584756956cfff2ad631bb622236
Leo Cho <bsjcho@gmail.com>
2017-10-25 16:40:21 -0700

    22. Generate Parentheses

--- leetcode.py
+++ leetcode.py
@@ -2378,3 +2378,25 @@ class Solution(object):
             nums1[i] = nums2[n]
             i -= 1
             n -= 1
+
+'''
+22. Generate Parentheses
+'''
+
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        validArrangements = []
+        def backtrack(string, opens, closes):
+            if len(string) == n*2:
+                validArrangements.append("".join(string))
+                return
+            if opens < n:
+                backtrack(string+["("], opens+1, closes)
+            if closes < opens:
+                backtrack(string+[")"], opens, closes+1)
+        backtrack([], 0, 0)
+        return validArrangements

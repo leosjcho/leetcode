58161d85a84be852823a95c4553123f039704249
Leo Cho <bsjcho@gmail.com>
2017-10-26 04:47:15 -0700

    49. Group Anagrams

--- leetcode.py
+++ leetcode.py
@@ -2485,3 +2485,29 @@ class Solution(object):
 
         emptyCount = countEmptySpots()
         backtrack(0, 0, emptyCount)
+
+'''
+49. Group Anagrams
+'''
+
+from collections import defaultdict
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        sorted_strs = ["".join(sorted(s)) for s in strs]
+        buckets = defaultdict(list)
+        for i, s in enumerate(sorted_strs):
+            buckets[s] = buckets[s] + [i]
+        anagrams = []
+        for bucket in buckets:
+            indices = buckets[bucket]
+            group = []
+            for i in indices:
+                group.append(strs[i])
+            anagrams.append(group)
+        return anagrams
+

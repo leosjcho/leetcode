979ffe0419a5bf135d7a01e8790b6928f141675b
Leo Cho <bsjcho@gmail.com>
2017-10-26 14:23:03 -0700

    160. Intersection of Two Linked Lists

--- leetcode.py
+++ leetcode.py
@@ -2562,4 +2562,39 @@ class Solution(object):
         t1.left = self.mergeTrees(t1.left, t2.left if t2 else None)
         t1.right = self.mergeTrees(t1.right, t2.right if t2 else None)
         return t1
-        
+
+'''
+160. Intersection of Two Linked Lists
+'''
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        initialA, initialB = headA, headB
+        aLooped = bLooped = False
+        while headA and headB:
+
+            if headA.val == headB.val:
+                return headA
+
+            headA = headA.next
+            headB = headB.next
+
+            if not headA and not aLooped:
+                headA = initialB
+                aLooped = True
+            if not headB and not bLooped:
+                headB = initialA
+                bLooped = True
+
+        return None
+    

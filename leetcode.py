bf033bcc7dfb72bf9279ef737d29982d524c2138
Leo Cho <bsjcho@gmail.com>
2017-10-26 04:47:26 -0700

    27. Remove Element

--- leetcode.py
+++ leetcode.py
@@ -2511,3 +2511,25 @@ class Solution(object):
             anagrams.append(group)
         return anagrams
 
+'''
+27. Remove Element
+'''
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        swap_index = len(nums)-1
+        swap_count = 0
+        i = 0
+        while i <= swap_index:
+            if nums[i] == val:
+                nums[i], nums[swap_index] = nums[swap_index], nums[i]
+                swap_index -= 1
+                swap_count += 1
+            else:
+                i += 1
+        return len(nums) - swap_count
